36398054a5cdc0200d1c3d3347d9a868be76e428
racimfethallah <ricdev.io@gmail.com>
2025-01-03 20:47:27 +0100

    fixed buttons

--- src/components/dashboard/NewProjectButton.tsx
+++ src/components/dashboard/NewProjectButton.tsx
@@ -5,7 +5,7 @@ export default function NewProjectButton() {
 
   return (
     <button
-      class="flex items-center mx-4 px-4 py-2 bg-purple-600 text-white rounded-lg hover:bg-purple-700"
+      class="flex items-center ml-4 px-4 py-2 bg-purple-600 text-white rounded-lg hover:bg-purple-700"
       onClick={() => {
         isNewProjectOpen.set(true);
       }}
